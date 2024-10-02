e76ebffdae7f9487a64ad8b3ca0b4f36a932eeda
Ashish kumar <vaibhavkumar1519@gmail.com>
2024-10-02 06:16:48 +0530

    api completed or backend completed

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -138,3 +138,11 @@ try {
     res.status(500).json({message:"server error",error:error.message});
 }
 }
+
+export const getProfile = async(req,res)=>{
+try {
+    res.status(200).json(req.user);
+} catch (error) {
+    res.status(500).json({message:"server error",error:error.message});
+}
+}
