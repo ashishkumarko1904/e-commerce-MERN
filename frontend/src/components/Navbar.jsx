a455f9e0fbfe40165e8b134053371ef73fcd1854
Ashish kumar <vaibhavkumar1519@gmail.com>
2024-10-05 06:44:56 +0530

    added navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,20 +1,34 @@
-import {Search,LogOut} from "lucide-react";
+import {Search,LogOut,ShoppingCart,House,LayoutDashboard} from "lucide-react";
 import {Link} from "react-router-dom";
-import "Navbar.css";
+import "./Navbar.css";
 const Navbar = () => {
   //for now lets say  
   //ToDo: fetch it from backend
   let isAdmin = true;
+  let itemCount = 1;
+  
   return (
     <div className="navbar">
-      <div>
-      <Search />
-      <input/>
+      <div className="search-container">
+      <Search color="whitesmoke"/>
+      <input className="search-input"/>
       </div>
-      {isAdmin&&<Link to='/dashboard'>Dashboard</Link>}
-      <Link>HOME</Link>
-      <Link>Cart</Link>
-      <button>logout<LogOut /></button>     
+     <div className="gp-navbar">
+     {isAdmin&&<Link className="link" to='/dashboard'>
+        <LayoutDashboard />
+      Dashboard
+      </Link>}
+      <Link className="link"><House />HOME</Link>
+      <Link className="link">Cart<ShoppingCart />{itemCount > 0 && (
+        <span className="cart-item-count">{itemCount}</span>
+      )}</Link>
+      <div className="btn-logout-container">
+      <button className="btn-logout">logout</button>   
+      <LogOut color="white"/>
+      
+      </div>
+      
+      </div>  
     </div>
   )
 }
